61a4e611aefd81b78ec993d1d6dd6f11f395415d
Saket Karn <saketkarn38@gmail.com>
2024-04-05 18:21:25 +0530

    Work done till social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
+import SocialLinks from './components/SocialLinks'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Home/>
+      <SocialLinks/>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,12 +27,17 @@ const NavBar = () => {
             <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 '>{link}</li>
         ))}
       </ul>
-      <div onClick={()=> setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500'>
+      <div onClick={()=> setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
             {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
       </div>
-      <ul className='flex flex-col justify-center'>
-      
+      {nav && (
+        <ul className='flex flex-col justify-center items-center absolute top-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+        {links.map(({id,link})=>(
+            <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{link}</li>
+        ))}      
+
       </ul>
+      )}
     </div>
   )
 }
